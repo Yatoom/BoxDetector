edbb2b5d5bf21f78f34c34527abaf18e18f525e6
Yatoom <jmapvhoof@gmail.com>
2017-08-25 18:22:14 +0200

    Get largest rectangle.

--- test.py
+++ test.py
@@ -28,38 +28,54 @@ contours = cv2.findContours(cannied.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_
 contours = contours[0] if imutils.is_cv2() else contours[1]
 sd = ShapeDetector()
 
-# loop over the contours
+# Find biggest contour
+biggest = None
+biggest_size = 0
 for contour in contours:
-    print("contour")
-    # compute the center of the contour, then detect the name of the
-    # shape using only the contour
-    M = cv2.moments(contour)
+    rect = cv2.minAreaRect(contour)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    new_size = cv2.contourArea(box)
 
+    # new_size = cv2.area(contour)
+    print(new_size)
+
+    # Check if contour does not start in top left
+    M = cv2.moments(contour)
     if M["m00"] == 0:
         continue
 
-    cX = int((M["m10"] / M["m00"]) * ratio)
-    cY = int((M["m01"] / M["m00"]) * ratio)
-    shape = sd.detect(contour)
-
-    rect = cv2.minAreaRect(contour)
-    box = cv2.boxPoints(rect)
-    box = box * ratio
-    box = np.int0(box)
-
-    # multiply the contour (x, y)-coordinates by the resize ratio,
-    # then draw the contours and the name of the shape on the image
-    contour = contour.astype("float")
-    contour *= ratio
-    contour = contour.astype("int")
-    cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-    rect = cv2.boundingRect(contour)
+    # Update if size is bigger
+    if new_size > biggest_size:
+        biggest_size = new_size
+        biggest = contour
 
 
+# compute the center of the contour, then detect the name of the
+# shape using only the contour
+M = cv2.moments(biggest)
 
-    cv2.putText(image, "{} ({})".format(shape, cv2.contourArea(contour)), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (255, 255, 255), 2)
+cX = int((M["m10"] / M["m00"]) * ratio)
+cY = int((M["m01"] / M["m00"]) * ratio)
 
-    # show the output image
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
+shape = sd.detect(biggest)
+
+rect = cv2.minAreaRect(biggest)
+box = cv2.boxPoints(rect)
+box = box * ratio
+box = np.int0(box)
+
+# multiply the contour (x, y)-coordinates by the resize ratio,
+# then draw the contours and the name of the shape on the image
+biggest = biggest.astype("float")
+biggest *= ratio
+biggest = biggest.astype("int")
+cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+rect = cv2.boundingRect(biggest)
+
+cv2.putText(image, "{} ({})".format(shape, cv2.contourArea(biggest)), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+            0.5, (255, 255, 255), 2)
+
+# show the output image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
