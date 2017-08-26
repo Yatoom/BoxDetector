591c4772403f86c84eedde09e08011aadd658336
Yatoom <jmapvhoof@gmail.com>
2017-08-26 16:58:38 +0200

    Better box detection with dilation, erosion, closing and opening.

--- test.py
+++ test.py
@@ -1,32 +1,44 @@
 import imutils
 import cv2
+
+import helpers
 from shape_detector import ShapeDetector
 from helpers import show_image
 import numpy as np
 
 # the shapes can be approximated better
-image = cv2.imread("images/1.png")
-resized = imutils.resize(image, width=100)
-ratio = image.shape[0] / float(resized.shape[0])
-back_resized = imutils.resize(image, width=100)
+image = cv2.imread("more_images/19.jpg")
+resized = imutils.resize(image, width=400)
+
+hsv_image = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
+h, s, v = cv2.split(hsv_image)
+# show_image("Hue", h)
+# show_image("Saturation", s)
+# show_image("Value", v)
+
+
+display_image = imutils.resize(image, width=400)
+ratio = display_image.shape[0] / float(resized.shape[0])
+back_resized = imutils.resize(image, width=400)
 # gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 # blurred = cv2.GaussianBlur(resized, (3, 3), 0)
-cannied = imutils.auto_canny(resized)
-# cannied = cv2.Canny(gray, 100, 200)
-_, cnts, _ = cv2.findContours(cannied, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cannied = imutils.auto_canny(v)
+dilation = cv2.dilate(cannied, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=5)
+closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
+erosion = cv2.erode(closing, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=3)
 
-show_image("Input image", image)
+# show_image("Input image", display_image)
 # show_image("Grayscale image", gray)
 # show_image("Blurred image", blurred)
 show_image("Cannied image", cannied)
+show_image("Closed image", erosion)
 
 # thresh = cv2.threshold(blurred, avg_gray_color, 255, cv2.THRESH_BINARY)[1]
 
 # find contours in the thresholded image and initialize the shape detector
-contours = cv2.findContours(cannied.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = cv2.findContours(erosion.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 contours = contours[0] if imutils.is_cv2() else contours[1]
-sd = ShapeDetector()
 
 # Find biggest contour
 biggest = None
@@ -37,45 +49,18 @@ for contour in contours:
     box = np.int0(box)
     new_size = cv2.contourArea(box)
 
-    # new_size = cv2.area(contour)
-    print(new_size)
-
     # Check if contour does not start in top left
     M = cv2.moments(contour)
     if M["m00"] == 0:
         continue
+
+    # helpers.show_contour(contour, display_image, ratio=ratio)
+    # helpers.show_box(contour, display_image, ratio=ratio)
 
     # Update if size is bigger
     if new_size > biggest_size:
         biggest_size = new_size
         biggest = contour
 
-
-# compute the center of the contour, then detect the name of the
-# shape using only the contour
-M = cv2.moments(biggest)
-
-cX = int((M["m10"] / M["m00"]) * ratio)
-cY = int((M["m01"] / M["m00"]) * ratio)
-
-shape = sd.detect(biggest)
-
-rect = cv2.minAreaRect(biggest)
-box = cv2.boxPoints(rect)
-box = box * ratio
-box = np.int0(box)
-
-# multiply the contour (x, y)-coordinates by the resize ratio,
-# then draw the contours and the name of the shape on the image
-biggest = biggest.astype("float")
-biggest *= ratio
-biggest = biggest.astype("int")
-cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-rect = cv2.boundingRect(biggest)
-
-cv2.putText(image, "{} ({})".format(shape, cv2.contourArea(biggest)), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-            0.5, (255, 255, 255), 2)
-
-# show the output image
-cv2.imshow("Image", image)
+helpers.show_box(biggest, display_image, ratio=ratio)
 cv2.waitKey(0)
